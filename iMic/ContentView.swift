fa3b15d14010f93cb4a398d32ebfccf3f4e7b1ed
Alex Broadhead <alex.broadhead@comcast.net>
2021-03-03 18:28:42 -0500

    Working (single) file player.  - Modified buttons from example to 'toggle'.

--- iMic/ContentView.swift
+++ iMic/ContentView.swift
@@ -6,11 +6,51 @@
 //
 
 import SwiftUI
+import AVKit
 
 struct ContentView: View {
+    @State var audioPlayer: AVAudioPlayer!
+    @State var showPlay: Bool = true
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("iMic").font(.system(size: 45)).font(.largeTitle)
+            
+            HStack {
+                if showPlay {
+                    Button(action: {
+                        play()
+                    }) {
+                        Image(systemName: "play.circle.fill").resizable()
+                            .frame(width: 100, height: 100)
+                            .aspectRatio(contentMode: .fit)
+                    }
+                }
+                if !showPlay {
+                    Button(action: {
+                        pause()
+                    }) {
+                        Image(systemName: "pause.circle.fill").resizable()
+                            .frame(width: 100, height: 100)
+                            .aspectRatio(contentMode: .fit)
+                    }
+                }
+            }
+        }
+        .onAppear {
+            let sound = Bundle.main.path(forResource: "Dun Ringill", ofType: "mp3")
+            self.audioPlayer = try! AVAudioPlayer(contentsOf: URL(fileURLWithPath: sound!))
+        }
+    }
+    
+    func play() {
+        self.audioPlayer.play()
+        self.showPlay = false
+    }
+    
+    func pause() {
+        self.audioPlayer.pause()
+        self.showPlay = true
     }
 }
 
