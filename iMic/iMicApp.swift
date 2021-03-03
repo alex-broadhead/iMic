7e1d1960dfea6b875b28e439ddc7eba0a23dae22
Alex Broadhead <alex.broadhead@comcast.net>
2021-03-03 17:02:10 -0500

    Initial Commit

---
+++ iMic/iMicApp.swift
@@ -0,0 +1,17 @@
+//
+//  iMicApp.swift
+//  iMic
+//
+//  Created by Alex Broadhead on 3/3/21.
+//
+
+import SwiftUI
+
+@main
+struct iMicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
