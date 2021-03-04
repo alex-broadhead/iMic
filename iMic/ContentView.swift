8c15c3ecb08b3ba1a3f2747059732acab9efb206
Alex Broadhead <alex.broadhead@comcast.net>
2021-03-03 19:30:14 -0500

    Add a delegate, so that we can handle callbacks.  - Loop file playback.

--- iMic/ContentView.swift
+++ iMic/ContentView.swift
@@ -9,26 +9,30 @@ import SwiftUI
 import AVKit
 
 struct ContentView: View {
-    @State var audioPlayer: AVAudioPlayer!
+    @State var audioHandler: AudioHandler = AudioHandler()
     @State var showPlay: Bool = true
-
+    
     var body: some View {
         VStack {
             Text("iMic").font(.system(size: 45)).font(.largeTitle)
             
             HStack {
+                // Place play and pause on top of each other, and toggle them
                 if showPlay {
                     Button(action: {
-                        play()
+                        audioHandler.play()
+                        showPlay = false
                     }) {
                         Image(systemName: "play.circle.fill").resizable()
                             .frame(width: 100, height: 100)
                             .aspectRatio(contentMode: .fit)
                     }
                 }
+                
                 if !showPlay {
                     Button(action: {
-                        pause()
+                        audioHandler.pause()
+                        showPlay = true
                     }) {
                         Image(systemName: "pause.circle.fill").resizable()
                             .frame(width: 100, height: 100)
@@ -37,25 +41,56 @@ struct ContentView: View {
                 }
             }
         }
+        
         .onAppear {
-            let sound = Bundle.main.path(forResource: "Dun Ringill", ofType: "mp3")
-            self.audioPlayer = try! AVAudioPlayer(contentsOf: URL(fileURLWithPath: sound!))
+            audioHandler.loadAudio()
         }
-    }
-    
-    func play() {
-        self.audioPlayer.play()
-        self.showPlay = false
-    }
-    
-    func pause() {
-        self.audioPlayer.pause()
-        self.showPlay = true
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+class AudioHandler: NSObject, ObservableObject, AVAudioPlayerDelegate {
+    @Published var audioPlayer: AVAudioPlayer!
+    @Published var isPlaying: Bool = false
+    
+    var myAudioPlayer = AVAudioPlayer()
+    
+    func play() {
+        myAudioPlayer.play()
+        isPlaying = true
+    }
+    
+    func pause() {
+        myAudioPlayer.pause()
+        isPlaying = false
+    }
+    
+    func loadAudio() {
+        let sound = Bundle.main.path(forResource: "Dun Ringill", ofType: "mp3")
+        do {
+            myAudioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: sound!))
+        } catch {
+            print("Couldn't load player!")
+        }
+        
+        if myAudioPlayer.delegate == nil {
+            myAudioPlayer.delegate = self
+        }
+    }
+    
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        print("Did finish playing!")
+        isPlaying = false
+        
+        if flag {
+            // For now, loop endlessly
+            loadAudio()
+            play()
+        }
     }
 }
